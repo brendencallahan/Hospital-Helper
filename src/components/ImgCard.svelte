01dd81395daa7bb214f95c32e826cc89ffbe6b63
Brenden Callahan <brendenkcallahan@gmail.com>
2022-08-03 18:44:58 -0700

    Got layout of cards ready

---
+++ src/components/ImgCard.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let src;
+    export let alt;
+    export let cardText;
+</script>
+
+<div>
+    {#if (src || alt)}
+    <img {src} {alt}/>
+    {/if}
+    <p>{cardText}</p>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,7 @@
-<p>Hello world</p>
+<script>
+    import ImgCard from '../components/ImgCard.svelte'
+</script>
+
+<ImgCard src='/graphics/main/Situp.svg' alt='a person laying down in bed' cardText='Incline Bed'/>
+
+<ImgCard cardText='Test'/>
