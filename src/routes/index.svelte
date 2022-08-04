4c4f199b0a57392207ece8b355ae71b9098cd022
Brenden Callahan <brendenkcallahan@gmail.com>
2022-08-03 19:58:30 -0700

    Added structure of pages as well as workaround for buttons to act as navigation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,26 @@
 <script>
-    import ImgCard from '../components/ImgCard.svelte'
+    import Card from '../components/Card.svelte'
 </script>
 
-<ImgCard src='/graphics/main/Situp.svg' alt='a person laying down in bed' cardText='Incline Bed'/>
+<div class="wrapper">
+    <Card src='/graphics/main/Situp.svg' alt='a person laying down in bed' cardText='Incline Bed'/>
+    <Card src='/graphics/main/Laydown.svg' alt='a person sitting up in bed' cardText='Lay down'/>
+    <Card src='/graphics/main/Sitting.svg' alt='a person sitting in a chair' cardText='Sit up'/>
+    <Card src='/graphics/main/Medicine.svg' alt='a medicine pill' cardText='Need Medicine'/>
+    <Card src='/graphics/main/Nurse.svg' alt='Nurse with a hat on' cardText='Nurse or Doctor'/>
+    <Card src='/graphics/main/Pain.svg' alt='two medicine pills overlapping' cardText='In Pain' pageLink='/pain'/>
+    <Card src='/graphics/main/Drink.svg' alt='a steaming bowl of soup' cardText='Food/Drink'/>
+    <Card src='/graphics/main/TV.svg' alt='a modern television' cardText='TV'/>
+    <Card src='graphics/main/Bath.svg' alt='a bathutub' cardText='Bath'/>
+</div>
 
-<ImgCard cardText='Test'/>
+<style>
+    .wrapper {
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: space-evenly;
+        padding: 0;
+        margin: 0;
+    }
+
+</style>
