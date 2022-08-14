f08ef75fdfa5e8c84395759871e48a5adce8ae0d
Brenden Callahan <brendenkcallahan@gmail.com>
2022-08-14 00:27:45 -0700

    Added layout for main page on mobile/desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '1/2w': '50vw',
+        '1/3w': '33.333vw',
+        '1/3h': '33.333vh',
+        '1/4h': '25vh',
+      }
+    },
   },
   plugins: [],
 }
