5b6eb4a1775172bfebd90057179f28eaab7ea62b
Brenden Callahan <brendenkcallahan@gmail.com>
2022-08-16 18:52:59 -0700

    Fixed responsiveness on small (height) screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
       spacing: {
-        '1/2w': '50vw',
-        '1/3w': '33.333vw',
-        '1w': '100vw',
-        '1/2h': '50vh',
-        '1/3h': '33.333vh',
-        '1/4h': '25vh',
+        '1/2w': 'max(185px, 50vw)',
+        '1/3w': 'max(185px, 33.333vw)',
+        '1w': 'max(185px, 100vw)',
+        '1/2h': 'max(185px, 50vh)',
+        '1/3h': 'max(185px, 33.333vh)',
+        '1/4h': 'max(185px, 25vh)',
       }
     },
   },
